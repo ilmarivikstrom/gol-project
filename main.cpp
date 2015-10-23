a2c05fc620195206225c875e8cfb562cfa569094
ilmarivikstrom <ilmarimikaelvikstrom@gmail.com>
2015-10-23 23:59:55 +0300

    Added little window pop-up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <unistd.h>
 
-int main(){
-	std::cout << "Runs." << std::endl;
+int main()
+{
+	int WIDTH = 200;
+	int HEIGHT = 200;
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of life v0.00");
+	
+	while(window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		
+		window.clear();
+		window.display();
+		usleep(10000);
+	}
+	
 	return 0;
 }
 
