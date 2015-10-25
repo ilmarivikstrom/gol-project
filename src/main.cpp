c87f9c4c66b452ec96c67b2432ad181695c230f4
ilmarivikstrom <ilmarimikaelvikstrom@gmail.com>
2015-10-25 13:01:56 +0200

    Removed junk

--- src/main.cpp
+++ src/main.cpp
@@ -42,67 +42,7 @@ int main()
 		window.display();
 	}
 	
-	
-	
 	return EXIT_SUCCESS;
-
-/*	THIS CODE IS HERE JUST FOR REFERENCE
-	sf::CircleShape triangle(120, 3);
-	sf::CircleShape rectangle(120, 4);
-	sf::CircleShape pentagon(120, 5);
-
-	#ifdef TEXTUREMODE
-	sf::Texture woodTexture;
-	if(!woodTexture.loadFromFile("res/woodtexture.png"))
-		return 1;
-		
-	sf::Texture rockTexture;
-	if(!rockTexture.loadFromFile("res/rocktexture.png"))
-		return 1;
 	
-	sf::Texture waterTexture;
-	if(!waterTexture.loadFromFile("res/watertexture.png"))
-		return 1;
-	
-	triangle.setTexture(&waterTexture);
-	rectangle.setTexture(&rockTexture);
-	pentagon.setTexture(&woodTexture);
-	#endif
-	
-	#ifdef COLORMODE
-	triangle.setFillColor(sf::Color(75, 75, 75));
-	rectangle.setFillColor(sf::Color(150, 150, 150));
-	pentagon.setFillColor(sf::Color(225, 225, 225));
-	#endif
-	
-	triangle.setPosition(200, 200);
-	rectangle.setPosition(400, 400);
-	pentagon.setPosition(600, 600);
-	
-	while(window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		
-		triangle.rotate(2);
-		rectangle.rotate(-2);
-		pentagon.rotate(2);
-		
-		
-		window.clear();
-		
-		window.draw(triangle);
-		window.draw(rectangle);
-		window.draw(pentagon);
-		
-		window.display();
-	}
-	
-	return EXIT_SUCCESS;
-	*/
 }
 
